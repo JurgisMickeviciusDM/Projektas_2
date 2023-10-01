1e83f71a3ae1b40f757f0c2f573edad4264ea201
Jurgis Mickevicius <144474535+JurgisMickeviciusDM@users.noreply.github.com>
2023-10-01 21:43:17 +0300

    Add files via upload
    Naudotojo įvdeimas perkeltas į atskirą funkciją

---
+++ Naudotojas.h
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <string>
+
+// Deklaruojame funkcijà
+void naudotojas(std::string& inputMethod, std::string& choice, std::string& header1, std::string& header2);
--- Projektas_1.cpp
+++ Projektas_1.cpp
@@ -11,49 +11,18 @@
 #include <algorithm>
 #include <cstdlib>
 #include <ctime>
-#include <fstream>
 #include <sstream>
 #include <numeric>
+#include "Naudotojas.h"
 
 using namespace std;
-
-//double Mediana.h
 
 int main() {
     srand(static_cast<unsigned int>(time(0)));
 
     string header1, header2, choice, inputMethod;  //kintamieji
+    naudotojas(inputMethod, choice, header1, header2);
     vector<Studentas> studentai; //vektorius
-
-
-    do {
-        cout << "Pasirinkite ar naudosite duomenis is failo, rasyti ,,Duomenys'' ar naudosite rankiniu budu ivedamus duomenis, rasyti ,,Ranka'': ";
-        cin >> inputMethod;
-        if (inputMethod != "Duomenys" && inputMethod != "Ranka") {
-            cout << "Neteisingai parasete! Bandykite dar karta." << endl;
-        }
-    } while (inputMethod != "Duomenys" && inputMethod != "Ranka");
-
-    do {
-        cout << "Prasome pasirinkti ka norite skaiciuoti vidurki ar mediana. Parasykite('Vidurkis') arba ('Mediana') arba ('ABU'):";
-        cin >> choice;
-
-        if (choice == "Vidurkis") {
-            header1 = "Galutinis(vid.)";
-        }
-        else if (choice == "Mediana") {
-            header1 = "Galutinis(med.)";
-        }
-        else if (choice == "ABU") {
-            header1 = "Galutinis(vid.)";
-            header2 = "Galutinis(med.)";
-        }
-        else {
-            cout << "Neteisinga pasirinkimas! Bandykite dar karta." << endl;
-        }
-
-    } while (choice != "Vidurkis" && choice != "Mediana" && choice != "ABU");
-    cin.ignore();
 
     int skaicius = 0;
     auto start = std::chrono::high_resolution_clock::now();
@@ -240,26 +209,26 @@ int main() {
         studentai.push_back(s);
     }
 
-    auto compareFromFile = [](const Studentas& a, const Studentas& b) {
-        int numA = stoi(a.pavarde.substr(7));
-        int numB = stoi(b.pavarde.substr(7));
-        return numA < numB;
+     auto compareFromFile = [](const Studentas& a, const Studentas& b) {
+       int numA = stoi(a.pavarde.substr(7));
+       int numB = stoi(b.pavarde.substr(7));
+       return numA < numB;
         };
 
-    auto simpleCompare = [](const Studentas& a, const Studentas& b) {
-        return a.pavarde < b.pavarde;
-        };
-
+     auto simpleCompare = [](const Studentas& a, const Studentas& b) {
+         return a.pavarde < b.pavarde;
+         };   
+    
     if (inputMethod == "Duomenys") {
 
-        sort(studentai.begin(), studentai.end(), compareFromFile);
+        sort(studentai.begin(), studentai.end(), compareFromFile); 
     }
     else {
         sort(studentai.begin(), studentai.end(), [](const Studentas& a, const Studentas& b) {
             return a.pavarde < b.pavarde;
-            });
+            });  
     }
-
+ 
     cout << fixed << setprecision(2);
     cout << setw(20) << left << "Pavarde"
         << setw(20) << left << "Vardas";
