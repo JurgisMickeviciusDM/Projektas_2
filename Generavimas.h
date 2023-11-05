29a3257d026e74802ed64a8b9e9d48e34c2a27cf
Jurgis Mickevicius <144474535+JurgisMickeviciusDM@users.noreply.github.com>
2023-11-05 11:16:18 +0200

    Delete Generavimas.h

--- Generavimas.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "Studentas.h"
-#include <vector>
-
-void Ifaila();
-void sortAndSave(const std::vector<Studentas>& students, const std::string& baseFilename);
-void saveToFile(const std::vector<Studentas>& students, const std::string& filename);
