fe8be5380c320fd0e767b561d630e405a72ba434
Jurgis Mickevicius <144474535+JurgisMickeviciusDM@users.noreply.github.com>
2023-11-10 09:42:56 +0200

    Update RUNMEE.bat

--- RUNMEE.bat
+++ RUNMEE.bat
@@ -3,5 +3,5 @@ BREAK>> RUNMEE.txt
 ECHO cmake CMakeLists.txt >> RUNMEE.txt
 ECHO cmake --install . >> RUNMEE.txt
 ECHO cmake --build . >> RUNMEE.txt
-ECHO copy src\studentai.txt Debug\studentai.txt >> RUNMEE.txt
+ECHO copy src\studentai1000.txt Debug\studentai.txt >> RUNMEE.txt
 ECHO Debug\Projektasv10.exe >> RUNMEE.txt
