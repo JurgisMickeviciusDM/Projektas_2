70fbd970d407304b9d1783dbba8f669d324d3414
Jurgis Mickevicius <144474535+JurgisMickeviciusDM@users.noreply.github.com>
2023-12-05 23:11:25 +0200

    Delete Naudotojas22.h

--- Naudotojas22.h
+++
@@ -1,9 +0,0 @@
-#ifndef NAUDOTOJAS22_H
-#define NAUDOTOJAS22_H
-
-#include <iostream>
-#include <string>
-
-void naudotojas(std::string& inputMethod, std::string& choice,std::string& choice1, std::string& choice2);
-#endif
-
